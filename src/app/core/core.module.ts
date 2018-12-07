acee5266fea1127cade473b2f96c448abc86c4fe
andbel <andbel2323@gmail.com>
2018-12-07 19:48:09 +0800

    create core/sidebar, banner component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CoreRoutingModule } from './core-routing.module';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { BannerComponent } from './banner/banner.component';
 
 @NgModule({
   imports: [
     CommonModule,
     CoreRoutingModule,
   ],
-  declarations: []
+  declarations: [SidebarComponent, BannerComponent]
 })
 export class CoreModule { }
